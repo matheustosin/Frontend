167f9711c852420ffcc3e4ab8d8b87b342cfbac0
Alexandre Scheer Bing <alexandrebing@gmail.com>
2020-04-05 19:34:13 -0300

    Hello world button working

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,51 +1,55 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from '../assets/logo.png';
 
 import './Login.css';
 
+class Login extends Component{
 
-function Login() {
-  return (
+  render(){
+    return (
 
-     <div className="Login">
-      <div className="login-background-container"></div>
-      <div className="login-panel-container">
+      <div className="Login">
+       <div className="login-background-container"></div>
+       <div className="login-panel-container">
+ 
+         <img  className='logo'
+               alt='rede-de-mentores-logo' 
+               src={logo}
+         />
+ 
+         <form className='login-panel-form'>
+             <div className='login-input-field'>
+               <label htmlFor="email-input">Email</label>
+               <input id="email-input" type='email'/>
+             </div>
+ 
+             <div className="login-input-field">
+               <label htmlFor="pw-input">Senha</label>
+               <input id="pw-input" type='password'/>
+             </div>
+ 
+             <a href='#'>Esqueci minha senha</a>
+           <button type="button" onClick={this.attemptLogin}>Entrar</button>
+         </form>
+ 
+         <div className="login-separator">
+           <hr/>
+             <h4>Novo por aqui?</h4>
+           <hr/>
+         </div>
+ 
+         <button type="button">Cadastrar</button>
+ 
+       </div>
+   </div>
+    )
 
-        <img  className='logo'
-              alt='rede-de-mentores-logo' 
-              src={logo}
-        />
+  }
 
-        <form className='login-panel-form'>
-            <div className='login-input-field'>
-              <label htmlFor="email-input">Email</label>
-              <input id="email-input" type='email'/>
-            </div>
+  attemptLogin = async () => {
+    console.log("Hello Function")
+  }
 
-            <div className="login-input-field">
-              <label htmlFor="pw-input">Senha</label>
-              <input id="pw-input" type='password'/>
-            </div>
-
-            <a href='#'>Esqueci minha senha</a>
-          <button type="button">Entrar</button>
-        </form>
-
-        <div className="login-separator">
-          <hr/>
-            <h4>Novo por aqui?</h4>
-          <hr/>
-        </div>
-
-        <button type="button">Cadastrar</button>
-
-      </div>
-  </div>
-  );
 }
-
-// attemptLogin = async () => {
-//   console.log("Hello Function")
-// }
 
 export default Login;
