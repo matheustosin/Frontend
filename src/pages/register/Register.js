6ac6b02fe574f44082d4ee1780fb46941fbd0c03
Carvalho <guilherme.carvalho@edu.pucrs.br>
2020-04-08 18:33:12 -0300

    page cadastro de usuario

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import logo from "../../assets/logo.png";
+import logo from "../../assets/logo2.png";
 import interview from "../../assets/interview.png";
 import teacher from "../../assets/teacher.png";
 
@@ -8,28 +8,44 @@ import "./Register.css";
 
 export default function Register() {
   return (
-    <div className="Register">
-      <div className="register-background-container"></div>
-      <div className="register-panel-container">
-        <img className="logo" alt="rede-de-mentores-logo" src={logo} />
-        <a href="#" className="login-user">
-          User
-        </a>
-      </div>
-      <div className="register-panel-form">
-        <div className="register-space">
-          <img className="logo" alt="rede-de-mentores-logo" src={teacher} />
-          <button type="button">CADASTRAR COMO MENTOR</button>
-        </div>
-
-        <div className="register-separator" />
-
-        <div className="register-space">
-          <img className="logo" alt="rede-de-mentores-logo" src={interview} />
-          <button type="button">CADASTRAR COMO ALUNO</button>
+    <React.Fragment>
+      <div className="register-background-container">
+        <div className="register-panel-container">
+          <img className="logo" alt="rede-de-mentores-logo" src={logo} />
         </div>
       </div>
-    </div>
-    
+
+      <div className="title">
+        <h1>COMO DESEJA SE CADASTRAR NA REDE?</h1>
+      </div>
+
+      <div className="Register">
+        <div className="register-panel-form">
+          <div className="register-space">
+            <div className="circle">
+              <img
+                className="image"
+                alt="rede-de-mentores-logo"
+                src={teacher}
+              />
+            </div>
+            <button type="button">CADASTRAR COMO MENTOR</button>
+          </div>
+
+          <div className="register-separator" />
+
+          <div className="register-space">
+            <div className="circle">
+              <img
+                className="image"
+                alt="rede-de-mentores-logo"
+                src={interview}
+              />
+            </div>
+            <button type="button">CADASTRAR COMO ALUNO</button>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
