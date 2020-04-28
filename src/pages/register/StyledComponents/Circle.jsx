7291d4436aca819b7d41724a2425644182babdda
luiza.pereira <luiza.pereira@ilegra.com>
2020-04-27 21:40:30 -0300

    Componentizando a tela de Escolha com a Eduarda

---
+++ src/pages/register/StyledComponents/Circle.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+import COLOR from '../../../utils/colors.constants';
+
+const Circle = styled.div`
+  width: 350px;
+  height: 350px;
+  display: flex;
+  align-items: center;
+  border-radius: 100%;
+  justify-content: center;
+  border: 2px solid ${COLOR.AZUL};
+
+  @media only screen and (max-width: 600px) {
+    width: 210px;
+    height: 210px;
+  }
+`;
+
+export default Circle;
