77c8182b95315eb18c6ab632d3e842f8b151bdef
DouglasCarboni <douglas.dallavale@acad.pucrs.br>
2020-05-01 18:43:07 -0300

    Reutiliza componentes (Douglas e Gustavo Vidaletti)

---
+++ src/components/RedeButton/StyledComponents/Cancelar.jsx
@@ -0,0 +1,15 @@
+import { css } from 'styled-components';
+
+import COLOR from '../../../utils/colors.constants';
+
+const Cancelar = css`
+  color: ${COLOR.AZUL};
+  background-color: ${COLOR.BRANCO};
+  border: solid 1px ${COLOR.AZUL};
+
+  :hover {
+    box-shadow: 0 0 15px ${COLOR.AZUL};
+  }
+`;
+
+export default Cancelar;
--- src/pages/cadastro-aprendiz/CadastroAprendiz.jsx
+++ src/pages/cadastro-aprendiz/CadastroAprendiz.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import Container from './StyledComponents';
+import RedeButton from '../../components/RedeButton/RedeButton';
 import RedeHeader from '../../components/RedeHeader/RedeHeader';
-import RedeTextField from '../../components/RedeTextField/RedeTextField'
+import RedeTextField from '../../components/RedeTextField/RedeTextField';
+import AccountImage from '../../assets/account.png';
+import RedeHorizontalSeparator from '../../components/RedeHorizontalSeparator/RedeHorizontalSeparator'
+
 
 const CadastroAprendiz = () => (
     <Container width="100vw">
         <RedeHeader title='cadastro de aprendiz'/>
+        <Container.Image src={AccountImage} width="100px" height="100px"/>
+        <RedeButton descricao="Adicionar Foto" onClick={undefined} claro={true}/>
 
-        <Container.TextFieldContainer>
+        <Container.TextContainer>
             <Container>
                 <RedeTextField descricao="Nome Completo" value='' onChange={undefined} />
                 <RedeTextField descricao="Data de Nascimento" value='' onChange={undefined} />
@@ -15,12 +21,20 @@ const CadastroAprendiz = () => (
                 <RedeTextField descricao="Telefone" value='' onChange={undefined} />
                 <RedeTextField descricao="Matrícula" value='' onChange={undefined} />
             </Container>
+
+            {/* Isso foi necessário pois o componente "RedeHorizontalSeparator"
+                não apresentou um comportamento responsivo em algumas resoluções*/}
+            <Container.SeparatorWrapper>
+                <RedeHorizontalSeparator/>
+            </Container.SeparatorWrapper>
+
             <Container>
                 <RedeTextField descricao="Email" value='' onChange={undefined} />
                 <RedeTextField descricao="Senha" value='' onChange={undefined} />
                 <RedeTextField descricao="Confirmar Senha" value='' onChange={undefined} />
+                <RedeButton descricao="Cadastrar" onClick={undefined} />
             </Container>
-        </Container.TextFieldContainer>
+        </Container.TextContainer>
     </Container>
 );
 
