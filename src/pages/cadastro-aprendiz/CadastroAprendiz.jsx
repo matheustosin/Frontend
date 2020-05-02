e5e5f86a26803816f3ab4793be5a139f075e8f6f
Gustavo Vidaletti <gustavo.vidaletti@dbccompany.com.br>
2020-05-02 12:42:03 -0300

    transformando em classe e utilizando State com Máscaras: (Vidaletti e Douglas)

--- src/pages/cadastro-aprendiz/CadastroAprendiz.jsx
+++ src/pages/cadastro-aprendiz/CadastroAprendiz.jsx
@@ -1,41 +1,79 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import Container from './StyledComponents';
 import RedeButton from '../../components/RedeButton/RedeButton';
 import RedeHeader from '../../components/RedeHeader/RedeHeader';
 import RedeTextField from '../../components/RedeTextField/RedeTextField';
 import AccountImage from '../../assets/account.png';
-import RedeHorizontalSeparator from '../../components/RedeHorizontalSeparator/RedeHorizontalSeparator'
+import {
+  formatCPF,
+  formatTelefone,
+  formatDataNascimento,
+  formatMatricula,
+} from '../../utils/maskUtils';
 
+class CadastroAprendiz extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nome: '',
+      dataNascimento: '',
+      cpf: '',
+      telefone: '',
+      matricula: '',
+      email: '',
+      senha: '',
+      confirmarSenha: '',
+    };
+  }
 
-const CadastroAprendiz = () => (
-    <Container width="100vw">
-        <RedeHeader title='cadastro de aprendiz'/>
-        <Container.Image src={AccountImage} width="100px" height="100px"/>
-        <RedeButton descricao="Adicionar Foto" onClick={undefined} claro={true}/>
+  render() {
+    const {
+      nome,
+      dataNascimento,
+      cpf,
+      telefone,
+      matricula,
+      email,
+      senha,
+      confirmarSenha,
+    } = this.state;
+
+    return (
+      <Container width="100vw">
+        <RedeHeader title="cadastro de aprendiz" />
+        <Container.Image src={AccountImage} width="100px" height="100px" style={{ marginBottom: '2vh' }} />
+        <RedeButton descricao="Adicionar Foto" onClick={() => { }} claro />
 
         <Container.TextContainer>
-            <Container>
-                <RedeTextField descricao="Nome Completo" value='' onChange={undefined} />
-                <RedeTextField descricao="Data de Nascimento" value='' onChange={undefined} />
-                <RedeTextField descricao="CPF" value='' onChange={undefined} />
-                <RedeTextField descricao="Telefone" value='' onChange={undefined} />
-                <RedeTextField descricao="Matrícula" value='' onChange={undefined} />
-            </Container>
+          <Container>
+            <RedeTextField descricao="Nome Completo" valor={nome} onChange={(evt) => this.setState({ nome: evt.target.value })} />
+            <RedeTextField descricao="Data de Nascimento" valor={dataNascimento} onChange={(evt) => this.setState({ dataNascimento: formatDataNascimento(evt.target.value) })} />
+            <RedeTextField descricao="CPF" valor={cpf} onChange={(evt) => this.setState({ cpf: formatCPF(evt.target.value) })} />
+            <RedeTextField descricao="Telefone" valor={telefone} onChange={(evt) => this.setState({ telefone: formatTelefone(evt.target.value) })} />
+          </Container>
 
-            {/* Isso foi necessário pois o componente "RedeHorizontalSeparator"
-                não apresentou um comportamento responsivo em algumas resoluções*/}
-            <Container.SeparatorWrapper>
-                <RedeHorizontalSeparator/>
-            </Container.SeparatorWrapper>
+          {/* Isso foi necessário pois o componente "RedeHorizontalSeparator"
+                não apresentou um comportamento responsivo em algumas resoluções */}
+          <Container.SeparatorWrapper>
+            {/* <RedeHorizontalSeparator /> */}
+          </Container.SeparatorWrapper>
 
-            <Container>
-                <RedeTextField descricao="Email" value='' onChange={undefined} />
-                <RedeTextField descricao="Senha" value='' onChange={undefined} />
-                <RedeTextField descricao="Confirmar Senha" value='' onChange={undefined} />
-                <RedeButton descricao="Cadastrar" onClick={undefined} />
-            </Container>
+          <Container>
+            <RedeTextField descricao="Matrícula" valor={matricula} onChange={(evt) => this.setState({ matricula: formatMatricula(evt.target.value) })} />
+            <RedeTextField descricao="Email" valor={email} onChange={(evt) => this.setState({ email: evt.target.value })} />
+            <RedeTextField descricao="Senha" valor={senha} onChange={(evt) => this.setState({ senha: evt.target.value })} />
+            <RedeTextField descricao="Confirmar Senha" valor={confirmarSenha} onChange={(evt) => this.setState({ confirmarSenha: evt.target.value })} />
+          </Container>
+
         </Container.TextContainer>
-    </Container>
-);
 
-export default CadastroAprendiz;
+        <Container style={{ marginTop: '2vh' }}>
+          <RedeButton descricao="Cadastrar" onClick={() => { }} />
+        </Container>
+      </Container>
+    );
+  }
+}
+
+export default withRouter(CadastroAprendiz);
