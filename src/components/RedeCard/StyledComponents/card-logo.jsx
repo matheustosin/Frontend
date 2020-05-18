0b3bfb5616d4143d7ca50fad7803a88a6486d696
johnnyloreano <laureano.pedrojoao@gmail.com>
2020-05-18 19:15:57 -0300

    Fixed image

--- src/components/RedeCard/StyledComponents/card-logo.jsx
+++ src/components/RedeCard/StyledComponents/card-logo.jsx
@@ -4,6 +4,10 @@ const CardLogo = styled.img`
 width: 40%;
 height: 100%;
 border-radius : 0px 0px 8px 8px;
+@media screen and (max-width:1000px){
+    width: 100%;
+    height: 70vh;
+  }
 `;
 
 export default CardLogo;
---
+++ src/components/RedeCard/StyledComponents/icons-wrapper.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import CardFooterContent from './card-footer-content';
+import SubTitle from './subtitle';
+
+const IconsWrapper = styled.div`
+  display:flex;
+  flex-direction:row;
+  align-items:center;
+`
+
+export default IconsWrapper;   
