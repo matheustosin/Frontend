0183d7f429d967ba7a43c39fb6fa6814182d9c06
larissafiorini <larissa.fiorini@hotmail.com>
2020-05-20 22:16:12 -0300

    Fix tamanho de imagem e texto

--- src/components/RedeCard/StyledComponents/card-description.jsx
+++ src/components/RedeCard/StyledComponents/card-description.jsx
@@ -2,10 +2,12 @@ import styled from 'styled-components';
 
 const CardDescription = styled.p`
     line-height:1.5em;
-    font-size: 1em;
     width: auto;
-    display:block;
-    flex-direction:row;
+    flex-direction: row;
+    padding: 5px 0px 5px 0px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
 `
 
 export default CardDescription;
--- src/components/RedeCard/StyledComponents/card-logo.jsx
+++ src/components/RedeCard/StyledComponents/card-logo.jsx
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
 const CardLogo = styled.img`
-width: 40%;
-height: 100%;
-border-radius : 0px 0px 8px 8px;
+width: 400px;
+height: 230px;
+border-radius : 0px 0px 0px 8px;
 @media screen and (max-width:1000px){
     width: 100%;
     height: 55vh;
