b996d54bc21f0dfbec083e8514226692d14dd11c
larissafiorini <larissa.fiorini@hotmail.com>
2020-05-20 22:59:28 -0300

    Merge RedeCard da branch ComponentizacaoMentor

--- src/components/RedeCard/StyledComponents/card-logo.jsx
+++ src/components/RedeCard/StyledComponents/card-logo.jsx
@@ -1,15 +1,13 @@
 import styled from 'styled-components';
 
 const CardLogo = styled.img`
-width: 40%;
-height: 100%;
-border-radius : 0px 0px 8px 8px;
+width: 400px;
+height: 230px;
+border-radius : 0px 0px 0px 8px;
 @media screen and (max-width:1000px){
     width: 100%;
     height: 55vh;
   }
 `;
-
-
 
 export default CardLogo;
