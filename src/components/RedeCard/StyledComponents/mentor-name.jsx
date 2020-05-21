565839f473e2a2f558b6e79057aa2fe919b541d1
larissafiorini <larissa.fiorini@hotmail.com>
2020-05-20 23:39:17 -0300

    Adicionando mentor-image e mentor-name no componente RedeCard

---
+++ src/components/RedeCard/StyledComponents/mentor-name.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import COLORS from '../../../utils/colors.constants';
+
+const MentorName = styled.p`
+    text-align: center;
+    font-size: 1.3em;
+    padding:5px;
+    color: ${COLORS.AZUL};
+`
+
+export default MentorName;
