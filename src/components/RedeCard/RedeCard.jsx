a70e27a2e3b74625945186606931cb31217e43bd
johnnyloreano <laureano.pedrojoao@gmail.com>
2020-05-27 01:22:08 -0300

    Mocked timeslot

--- src/components/RedeCard/RedeCard.jsx
+++ src/components/RedeCard/RedeCard.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { string, func, bool } from 'prop-types';
 import Container from './StyledComponents';
-import visible from '../../assets/visibility-button.png';
+import visibility from '../../assets/visibility-button.png';
 import remove from '../../assets/rubbish-bin-delete-button.png';
 import edition from '../../assets/create-new-pencil-button.png';
-import notVisible from '../../assets/invisible-button.png';
 import RedeTimeSlot from '../RedeTimeSlot/RedeTimeSlot';
 import RedeIcon from '../RedeIcon/RedeIcon';
 import CardDescription from './StyledComponents/card-description';
@@ -19,39 +18,42 @@ import TimeSlotWrapper from './StyledComponents/timeslot-wrapper';
 const Card = ({
   title, description, image, visibleFunction, removeFunction, editFunction, isVisible, TimeSlots,
 }) => (
-    <Container>
-      <CardLogo src={image} />
-      <CardContent>
-        <CardHeader>
-          <CardHeader.Title>
-            {title}
-          </CardHeader.Title>
-          <CardHeader.Button descricao="TODOS HORÁRIOS" onClick={() => { alert('test'); }} />
-        </CardHeader>
-        <CardDescription>
-          {description}
-        </CardDescription>
-        <CardFooter>
-          <CardFooter.SubTitle>
-            Próximos horários
+  <Container visible={isVisible}>
+    <CardLogo src={image} />
+    <CardContent>
+      <CardHeader>
+        <CardHeader.Title>
+          {title}
+        </CardHeader.Title>
+        <CardHeader.Button descricao="TODOS HORÁRIOS" onClick={() => { }} />
+      </CardHeader>
+      <CardDescription>
+        {description}
+      </CardDescription>
+      <CardFooter>
+        <CardFooter.SubTitle>
+          Próximos horários
         </CardFooter.SubTitle>
-          <CardFooter.Content>
-            <TimeSlotWrapper>
-              {TimeSlots}
-            </TimeSlotWrapper>
-            <IconsWrapper>
-              <CardHeader.Button descricao="TODOS HORÁRIOS" claro="true" onClick={() => { alert('test'); }} />
-              <div>
-                <RedeIcon imageUrl={remove} onClick={removeFunction} />
-                <RedeIcon imageUrl={isVisible ? visible : notVisible} onClick={visibleFunction} />
-                <RedeIcon imageUrl={edition} onClick={editFunction} />
-              </div>
-            </IconsWrapper>
-          </CardFooter.Content>
-        </CardFooter>
-      </CardContent>
-    </Container>
-  );
+        <CardFooter.Content>
+          <TimeSlotWrapper>
+            <RedeTimeSlot descricao="SEG - 12:00" />
+            <RedeTimeSlot descricao="TER - 12:00" />
+            <RedeTimeSlot descricao="QUA - 12:00" />
+            {TimeSlots}
+          </TimeSlotWrapper>
+          <IconsWrapper>
+            <CardHeader.Button descricao="TODOS HORÁRIOS" onClick={() => { alert('test'); }} />
+            <div>
+              <RedeIcon imageUrl={remove} onClick={removeFunction} />
+              {/* <RedeIcon imageUrl={visibility} onClick={visibleFunction} /> */}
+              <RedeIcon imageUrl={edition} onClick={editFunction} />
+            </div>
+          </IconsWrapper>
+        </CardFooter.Content>
+      </CardFooter>
+    </CardContent>
+  </Container>
+);
 
 Card.propTypes = {
   description: string,
