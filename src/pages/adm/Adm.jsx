f5f897c5bfbe9b636c9c8762d1022c301668b9fe
Matheus <matheus.tosin@edu.pucrs.br>
2020-06-05 18:44:10 -0300

    mostrar icones e trocar piloto

--- src/pages/adm/Adm.jsx
+++ src/pages/adm/Adm.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import RedeHeader from '../../components/RedeHeader/RedeHeader';
 import Card from '../../components/RedeCard/RedeCard';
+import edition from '../../assets/create-new-pencil-button.png';
+import confirm from '../../assets/confirm.png';
+import denied from '../../assets/denied.png';
 import Container from './StyledComponents';
 import { urlFiles } from '../../services/http';
 import { mentoriasByMentorado } from '../../services/mentorado';
@@ -8,69 +11,14 @@ import RedeIcon from '../../components/RedeIcon/RedeIcon';
 
 function Administrador() {
     
-    const [cards, setCards] = useState('');
-    const [areaConhecimento, setAreaConhecimento] = useState(sessionStorage.getItem('areaSelected'));
-    const [mentorias, setMentorias] = useState([]);
-
-    useEffect(()=>{
-        const token = sessionStorage.getItem('token');
-        const headers = { headers: { Authorization: `Bearer ${token}` } };
-        
-        getMentorias(headers);
-    }, [])
-
-    function attemptSearch(event) { 
-        const searchCards = mentorias.filter(function (e) {
-            return e.title.toLowerCase().indexOf(event.toLowerCase()) !== -1;
-        });
-        generateCards(searchCards);
-    }
-
-    function getMentorias(headers) {
-        
-        mentoriasByMentorado(headers) 
-        .then((res) => {
-            if (res.status === 200) {
-                    filterMentorias(res.data);
-                }
-            })
-            .catch((err) => {
-                setCards('Nenhuma mentoria encontrada para a Área de Conhecimento selecionada.');
-                console.error(err);
-            });
-    }
-
-    function filterMentorias(arrayMentoriasAll) {
-
-        const mentoriasAreaConhecimento = arrayMentoriasAll
-        .filter(function (e) {
-            return e.knowledgeArea == areaConhecimento;
-        });
-        setMentorias(mentoriasAreaConhecimento);
-        generateCards(mentoriasAreaConhecimento);
-    }
-
-    function generateCards(mentoriasAreaConhecimento) {
-
-        const cardsMentorias = mentoriasAreaConhecimento
-        .map((mentoria) => (  
-            <Card 
-                title={mentoria.title}
-                description={mentoria.description}
-                image={`${urlFiles}/${mentoria.image}`}
-                mentorias={true}
-            />
-        ));
-        setCards(cardsMentorias);
-    }
-    
     return (
         <Container>
             <RedeHeader />
             <Container.Title>APROVAÇÕES PENDENTES</Container.Title>
             <br/>
-            <RedeIcon imageUrl={edition} />
-            {cards}
+            <RedeIcon imageUrl={confirm} onClick={} />
+            <RedeIcon imageUrl={denied} onClick={} />
+            <RedeIcon imageUrl={edition} onClick={} />
         </Container>
     );  
 }
