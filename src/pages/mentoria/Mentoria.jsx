9d0fd2470e9cd7bb8a4ec063f31e8925149b750e
DouglasCarboni <douglas.dallavale@acad.pucrs.br>
2020-06-08 19:17:42 -0300

    estruturando layout com componentes disponíveis

--- src/pages/mentoria/Mentoria.jsx
+++ src/pages/mentoria/Mentoria.jsx
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from './StyledComponents';
-//import Caminho from './StyledComponents/Caminho';
+import Caminho from '../mentorias/StyledComponents/Caminho';
+import CaminhoTitle from '../mentorias/StyledComponents/CaminhoTitle';
+import CaminhoTitleDesabilitado from '../mentorias/StyledComponents/CaminhoTitleDesabilitado';
+import CaminhoAp from '../mentorias/StyledComponents/CaminhoAp';
 import RedeHeader from '../../components/RedeHeader/RedeHeader';
+import RedeCard from '../../components/RedeCard/RedeCard';
 
 function Mentoria() {
+
+ // const [areaConhecimento, setAreaConhecimento] = useState(sessionStorage.getItem('areaSelected'));
+
   return (
     <Container>
       <RedeHeader />
-      {/* <CaminhoAp>
-      <CaminhoTitleDesabilitado>
-        <a href="../aprendiz/Aprendiz.jsx">Home</a>
-      </CaminhoTitleDesabilitado>
-      <Caminho />
-      <CaminhoTitle>{areaConhecimento}</CaminhoTitle>
-      </CaminhoAp> */}
-
+         <CaminhoAp>
+          <CaminhoTitleDesabilitado>
+            <a href="../aprendiz/Aprendiz.jsx">Home</a>
+          </CaminhoTitleDesabilitado>
+          <Caminho />
+          <CaminhoTitleDesabilitado>
+            <a href="../aprendiz/Aprendiz.jsx">Área do Conhecimento</a>
+          </CaminhoTitleDesabilitado>
+          <Caminho />
+          <CaminhoTitle>{'Nome da Mentoria'}</CaminhoTitle>
+         </CaminhoAp>
+         <RedeCard></RedeCard>
     </Container>
   );
 }
