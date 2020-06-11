5484381c9d7535eb82b2b99507ce276b9fbf8626
Ardel Junior <ardeljunior@gmail.com>
2020-06-11 18:34:11 -0300

    removi comentário inútil

--- src/utils/validationUtils.js
+++ src/utils/validationUtils.js
@@ -1,6 +1,5 @@
 export const validateEmail = (value) => {
 
-    //const re = /^(([^<>()[]\.,;:\s@"]+(.[^<>()[]\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
     const re = /^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/;
     console.log(re.test(value));
     return re.test(value);
