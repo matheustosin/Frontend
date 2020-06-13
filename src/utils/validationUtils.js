e094f85d5d4cce1bc88f7cee5635f09e8f787e46
Ardel Junior <ardeljunior@gmail.com>
2020-06-13 12:02:00 -0300

    fix

--- src/utils/validationUtils.js
+++ src/utils/validationUtils.js
@@ -1,6 +1,6 @@
 export const validateEmail = (value) => {
 
-    const re = /^(?:[a-zA-Z0-9]+\.)+[A-Za-z]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/;
+    const re = /^[a-z0-9]+[.]?[a-z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/;
     console.log(re.test(value));
     return re.test(value);
 }
