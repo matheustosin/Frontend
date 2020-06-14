f0dc1dd814b67091ab741f3c98a79a8deb51b790
larissafiorini <larissa.fiorini@hotmail.com>
2020-06-13 22:10:06 -0300

    removendo comentario

--- src/utils/validationUtils.js
+++ src/utils/validationUtils.js
@@ -1,5 +1,4 @@
 export default function validateEmail(value) {
   const re = /^(?:[a-z0-9]+[.!#$%&'*+-/=?^_`{|}~]?)+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/;
-  // console.log(re.test(value));
   return re.test(value);
 }
