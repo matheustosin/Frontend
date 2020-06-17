3470fd9a68660b4b4df9b6cac9fe4f49cf630be5
DouglasCarboni <douglas.dallavale@acad.pucrs.br>
2020-06-17 17:07:09 -0300

    Correção parcial dos botões do calendário

--- src/components/RedeHorarioCard/RedeHorarioCard.jsx
+++ src/components/RedeHorarioCard/RedeHorarioCard.jsx
@@ -14,13 +14,15 @@ function RedeHorarioCard({ mentoria }) {
   const [dateInfo, setDateInfo] = React.useState('');
 
   const { dateTime } = mentoria;
-  const sortedTimes = dateTime.sort((dateTimeA, dateTimeB) => dateTimeA.dayOfTheMonth.split('/')[0] - dateTimeB.dayOfTheMonth.split('/')[0])
+  const sortedDates = dateTime
+    .sort((dateTimeA, dateTimeB) => dateTimeA.dayOfTheMonth.split('/')[0] - dateTimeB.dayOfTheMonth.split('/')[0])
     .sort((dateTimeA, dateTimeB) => dateTimeA.dayOfTheMonth.split('/')[1] - dateTimeB.dayOfTheMonth.split('/')[1]);
 
-  const timeInformation = sortedTimes.map((dt) => {
+  const timeInformation = sortedDates.map((dt) => {
     // eslint-disable-next-line max-len
     const hours = dt.times.map((time) => (
       <RedeHorarioButton
+        ocupado={time.flagBusy}
         horario={time.hour}
         onClick={() => {
           setTimeInfo(time.hour);
@@ -31,7 +33,7 @@ function RedeHorarioCard({ mentoria }) {
     ));
 
     return (
-      <Details>
+      <Details key={dt.dayOfTheMonth}>
         <Label>{dt.dayOfTheMonth}</Label>
         <Hours>
           {hours}
@@ -44,7 +46,13 @@ function RedeHorarioCard({ mentoria }) {
     const token = sessionStorage.getItem('token');
     const headers = { headers: { Authorization: `Bearer ${token}` } };
     marcarMentoria(headers, { idMentoria: mentoria.idMentoria, choice: data })
-      .then((res) => (res.status === 200 ? console.log('mentoria marcada') : console.log('Falha ao marcar mentoria. Código: ', res.status)))
+      .then((res) => {
+        if (res.status === 200) {
+          console.log('mentoria marcada');
+        } else {
+          console.log('Falha ao marcar mentoria. Código: ', res.status);
+        }
+      })
       .catch((err) => console.error(err));
   }
 
