93e58f7f1dd2ea3a2cb4cc95cf06ebab24be9efa
Luis Santana <luis.santana@kinghost.com.br>
2020-06-17 18:20:26 -0300

    Ajuste assim que cadatrar o usuario mandar pra home

--- src/pages/cadastro-mentorado/CadastroMentorado.jsx
+++ src/pages/cadastro-mentorado/CadastroMentorado.jsx
@@ -114,10 +114,7 @@ function CadastroMentorado() {
         .then((res) => {
           if (res.status === 200) {
             enqueue('Usuário cadastrado com sucesso!', 'success');
-            pushIfNecessary(
-              userTypes.MENTORADO,
-              (link) => history.push(link),
-            );
+            history.push('/');
           }
         })
         .catch((err) => {
